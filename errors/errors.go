4d2ff7b0fb6e2d55d267093dd85145509e23b8c9
zacksfF <ssssaasss571@gmail.com>
2024-06-18 22:26:18 +0100

    Inital errors

---
+++ errors/errors.go
@@ -0,0 +1,24 @@
+package errors
+
+// eqldbError is returned when an error caused by EQLDB has occurred.
+type eqldbError struct {
+	errormessage string
+}
+
+// Return the message denoting the cause of the error
+func (e *eqldbError) Error() string {
+	return e.errormessage
+}
+
+type TxnError struct {
+	transactionID string
+	message       string
+	err           error
+	canRetry      bool
+	abortSuccess  bool
+	isISE         bool
+}
+
+func (e *TxnError) Unwrap() error {
+	return e.err
+}

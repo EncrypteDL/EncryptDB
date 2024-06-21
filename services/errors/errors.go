3d28145ef4c110c88a45bc3443e99aa2c7ed985e
zacksfF <ssssaasss571@gmail.com>
2024-06-21 22:44:49 +0100

    @~ Add errors

---
+++ services/errors/errors.go
@@ -0,0 +1,24 @@
+package errors
+
+type txnError struct {
+	transactionID string
+	message       string
+	err           error
+	canRetry      bool
+	abortSuccess  bool
+	isISE         bool
+}
+
+// qldbDriverError is returned when an error caused by QLDBDriver has occurred.
+type eqldbDriverError struct {
+	errorMessage string
+}
+
+// Return the message denoting the cause of the error.
+func (e *eqldbDriverError) Error() string {
+	return e.errorMessage
+}
+
+func (e *txnError) unwrap() error {
+	return e.err
+}
